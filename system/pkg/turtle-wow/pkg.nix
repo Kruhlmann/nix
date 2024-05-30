a535d5ec7755e3df72323586afd782f942417842
Andreas Krühlmann <andreas@kruhlmann.dev>
2024-05-30 20:08:42 +0200

    Add desktop entry for turtle wow

--- system/pkg/turtle-wow/pkg.nix
+++ system/pkg/turtle-wow/pkg.nix
@@ -7,24 +7,31 @@ pkgs.stdenv.mkDerivation rec {
     url = "https://turtle-eu.b-cdn.net/twmoa_${version}.zip";
     sha256 = "sha256-fJNiSRln7xsyv3P4B6NTudvF7wAVFSJdu2LEpPdqu3w=";
   };
-  nativeBuildInputs = [ pkgs.unzip pkgs.copyDesktopItems ];
+  icon = pkgs.fetchurl {
+    url =
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5kziZqFv4kii1P8K8MYnl3XkME-ZF2TWSf6T2uPWArDYLYDEn";
+    sha256 = "sha256-0XdP81dd9InEAhdFkR5QfUIbcwn3zSKQJeCPURJUZh0=";
+  };
+  nativeBuildInputs = [ pkgs.unzip pkgs.copyDesktopItems pkgs.gtk3 ];
   buildInputs = [ pkgs.wineWowPackages.full pkgs.vulkan-loader ];
   dontBuild = true;
   installPhase = ''
-    mkdir -p $out/share/turtle-wow $out/bin
+    mkdir -p $out/share/turtle-wow $out/bin $out/share/icons/hicolor/256x256/apps/ $out/share/applications
     cp -r * $out/share/turtle-wow
+    cp ${icon} $out/share/icons/hicolor/256x256/apps/turtle-wow.png
+    cp -r ${desktop}/share/applications/* $out/share/applications
     echo "#!/usr/bin/env sh" > $out/bin/turtle-wow
     echo "WINEPREFIX="~/.wine-turtle-wow" WINEARCH=win32 wine $out/share/turtle-wow/WoW.exe" >> $out/bin/turtle-wow
     chmod +x $out/bin/turtle-wow
   '';
-  desktopItems = [
-    (pkgs.makeDesktopItem {
-      name = "turtle-wow";
-      exec = "turtle-wow";
-      desktopName = "Play Turtle WoW";
-      categories = [ "Game" ];
-    })
-  ];
+  desktop = pkgs.makeDesktopItem {
+    type = "Application";
+    name = "turtle-wow";
+    exec = "turtle-wow";
+    icon = "turtle-wow";
+    desktopName = "Play Turtle WoW";
+    categories = [ "Game" ];
+  };
   meta = with pkgs.lib; {
     description = "Turtle WoW - A private server for World of Warcraft";
     homepage = "https://turtle-wow.org";
