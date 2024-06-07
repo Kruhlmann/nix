dc170a93094f29e8b7df065ca36401c6bf0ecb7e
Andreas Krühlmann <andreas@kruhlmann.dev>
2024-06-07 10:28:37 +0200

    Move config

--- system/configuration.nix
+++ system/configuration.nix
@@ -1,56 +1,13 @@
 { pkgs, ... }:
 let
-  wow = import ./pkg/turtle-wow/wow-types.nix { };
-  servers = import ./pkg/turtle-wow/servers.nix { };
-  addons = import ./pkg/turtle-wow/addons.nix { };
+  turtle-wow-config = import ./turtle-wow.nix {};
   turtle-wow = import ./pkg/turtle-wow/default.nix {
     inherit pkgs;
-    ver = "1171";
-    gameConfig = {
-      MusicVolume = "0.4";
-      MasterVolume = "0.1";
-      realmName = servers.turtleWow.realms.RP.Nordanaar;
-      realmList = servers.turtleWow.realmList;
-      patchlist = servers.turtleWow.patchlist;
-      accountName = "ges";
-      UnitNameOwn = wow.true;
-      cameraPivot = wow.false;
-      showGameTips = wow.false;
-      cameraDistanceMaxFactor = "2";
-    };
-    accountConfigs = { ges = { AUTO_QUEST_WATCH = wow.false; }; };
-    addons = [
-      addons.balakethelock.twthreat
-      addons.berranzan.modifiedpowerauras-continued
-      addons.doorknob6.pfui-turtle
-      addons.firenahzku.vgattackbar
-      addons.hosq.bigwigs
-      addons.isitlove.outfitter
-      addons.jsb.gatherer
-      addons.kakysha.honorspy
-      addons.kxseven.vanillaratingbuster
-      addons.lexiebean.atlasloot
-      addons.mrrosh.healcomm
-      addons.mrrosh.mcp
-      addons.road-block.classicsnowfall
-      addons.shagu.pfquest
-      addons.shagu.pfquest-turtle
-      addons.shagu.pfui
-      addons.shagu.shagudps
-      addons.shirsig.aux-addon-vanilla
-      addons.shirsig.aux_merchant_prices
-      addons.shirsig.unitscan
-      addons.shirsig.wim
-      addons.satan666.trinketmenu-fix
-      addons.opcow.buffreminder
-      addons.danieladolfsson.clevermacro
-      addons.tercioo.details-damage-meter
-      addons.wow-vanilla-addons.accountant
-      addons.wow-vanilla-addons.postal
-      addons.yogo1212.healbot-classic
-      addons.fiskehatt.saysapped
-      addons.proxicide.macroextender
-    ];
+    ver = turtle-wow-config.ver;
+    gameConfig = turtle-wow-config.gameConfig;
+    accountConfigs = turtle-wow-config.accountConfigs;
+    addons = turtle-wow-config.addons;
+    macros = turtle-wow-config.macros;
   };
 in {
   imports = [
--- system/pkg/turtle-wow/default.nix
+++ system/pkg/turtle-wow/default.nix
@@ -1,6 +1,6 @@
 { pkgs, ver ? "1171", addons ? [ ]
 , winePrefix ? "~/.cache/turtle-wow/.wine-prefix", gameConfig ? { }
-, accountConfigs ? { } }:
+, accountConfigs ? { }, macros ? { } }:
 
 let
   buildAddon = import ./addons/build.nix { inherit pkgs; };
@@ -12,7 +12,6 @@ let
     settings = combinedConfig;
     useDoubleQuotes = true;
   };
-
   defaultAccountConfig = import ./config/preset/account.nix { };
   mappedAccountConfigs = pkgs.lib.mapAttrs (accountName: accountConfig:
     pkgs.lib.recursiveUpdate defaultAccountConfig accountConfig) accountConfigs;
@@ -22,6 +21,16 @@ let
       settings = accountConfig;
       useDoubleQuotes = false;
     });
+  generateMacroConfig = accountName: macroConfig:
+    pkgs.writeText "macros-cache.txt" (import ./macro.nix {
+      inherit pkgs;
+      macros = macroConfig;
+    });
+  mappedMacroConfigs = pkgs.lib.mapAttrs (groupName: macroConfig:
+    pkgs.writeText "${groupName}-macros-cache.txt" (import ./macro.nix {
+      inherit pkgs;
+      macros = macroConfig;
+    })) macros;
 in pkgs.stdenv.mkDerivation rec {
   pname = "turtle-wow";
   version = "${ver}";
@@ -33,40 +42,45 @@ in pkgs.stdenv.mkDerivation rec {
   buildInputs = [ pkgs.wineWowPackages.full pkgs.vulkan-loader ];
   dontBuild = true;
   installPhase = ''
-      mkdir -p $out/share/turtle-wow $out/bin $out/share/icons/hicolor/256x256/apps/ $out/share/applications
-      rm -f ./realmlist.wtf
-      cp -r * $out/share/turtle-wow
-      cp ${
-        pkgs.copyPathToStore ./res/icon.png
-      } $out/share/icons/hicolor/256x256/apps/turtle-wow.png
-      cp -r ${desktop}/share/applications/* $out/share/applications
-      ${
-        pkgs.lib.concatMapStrings (addon: ''
-          cp -r ${addon}/share/${addon.pname} $out/share/turtle-wow/Interface/AddOns/
-        '') buildAddons
-      }
-      substituteAll ${./res/turtle-wow} $out/bin/turtle-wow
-      echo "export WINE_STORE=${pkgs.wineWowPackages.full}" >>$out/bin/turtle-wow.env
-      echo "export TURTLE_WOW_STORE=$out" >>$out/bin/turtle-wow.env
-      echo "export WINEPREFIX=${winePrefix}" >>$out/bin/turtle-wow.env
-      chmod +x $out/bin/turtle-wow
-      printf 'set realmlist %s\nset patchlist %s\n' "${combinedConfig.realmList}" "${combinedConfig.patchlist}" >$out/share/turtle-wow/realmlist.wtf
-      echo "${generatedWtfConfig}" >$out/share/turtle-wow/WTF/Config.wtf
-      chmod 444 $out/share/turtle-wow/WTF/Config.wtf  
-    ${
-      pkgs.lib.concatStrings (pkgs.lib.mapAttrsToList
-        (accountName: accountConfig:
-          let upperAccountName = pkgs.lib.toUpper accountName;
-          in ''
-            mkdir -p $out/share/turtle-wow/WTF/Account/${upperAccountName}    
-            cp ${
-              generateAccountConfig accountName accountConfig
-            } $out/share/turtle-wow/WTF/Account/${upperAccountName}/SavedVariables.lua    
-            chmod 444 $out/share/turtle-wow/WTF/Account/${upperAccountName}/SavedVariables.lua  
-          '') mappedAccountConfigs)
-    }    
-      mv $out/share/turtle-wow/WTF $out/share/turtle-wow/WTF.original
-      ln -s /tmp/turtle-wow $out/share/turtle-wow/WTF
+          mkdir -p $out/share/turtle-wow $out/bin $out/share/icons/hicolor/256x256/apps/ $out/share/applications
+          rm -f ./realmlist.wtf
+          cp -r * $out/share/turtle-wow
+          cp ${
+            pkgs.copyPathToStore ./res/icon.png
+          } $out/share/icons/hicolor/256x256/apps/turtle-wow.png
+          cp -r ${desktop}/share/applications/* $out/share/applications
+          ${
+            pkgs.lib.concatMapStrings (addon: ''
+              cp -r ${addon}/share/${addon.pname} $out/share/turtle-wow/Interface/AddOns/
+            '') buildAddons
+          }
+          cp ${pkgs.copyPathToStore ./res/turtle-wow} $out/bin/turtle-wow
+          echo "export WINE_STORE=${pkgs.wineWowPackages.full}" >>$out/bin/turtle-wow.env
+          echo "export TURTLE_WOW_STORE=$out" >>$out/bin/turtle-wow.env
+          echo "export WINEPREFIX=${winePrefix}" >>$out/bin/turtle-wow.env
+          chmod +x $out/bin/turtle-wow
+          printf 'set realmlist %s\nset patchlist %s\n' "${combinedConfig.realmList}" "${combinedConfig.patchlist}" >$out/share/turtle-wow/realmlist.wtf
+          echo "${generatedWtfConfig}" >$out/share/turtle-wow/WTF/Config.wtf
+          chmod 444 $out/share/turtle-wow/WTF/Config.wtf
+    ${pkgs.lib.concatStrings (pkgs.lib.mapAttrsToList (groupName: macroFile: ''
+      mkdir -p $out/share/turtle-wow/WTF/Account/${groupName}
+      cp ${macroFile} $out/share/turtle-wow/WTF/Account/${groupName}/macros-cache.txt
+      chmod 444 $out/share/turtle-wow/WTF/Account/${groupName}/macros-cache.txt
+    '') mappedMacroConfigs)}
+        ${
+          pkgs.lib.concatStrings (pkgs.lib.mapAttrsToList
+            (accountName: accountConfig:
+              let upperAccountName = pkgs.lib.toUpper accountName;
+              in ''
+                mkdir -p $out/share/turtle-wow/WTF/Account/${upperAccountName}    
+                cp ${
+                  generateAccountConfig accountName accountConfig
+                } $out/share/turtle-wow/WTF/Account/${upperAccountName}/SavedVariables.lua    
+                chmod 444 $out/share/turtle-wow/WTF/Account/${upperAccountName}/SavedVariables.lua  
+              '') mappedAccountConfigs)
+        }    
+          mv $out/share/turtle-wow/WTF $out/share/turtle-wow/WTF.original
+          ln -s /tmp/turtle-wow $out/share/turtle-wow/WTF
   '';
   desktop = pkgs.makeDesktopItem {
     type = "Application";
