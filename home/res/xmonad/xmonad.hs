550bd7f688da992672d965ab7aacbf3d9e7f9a3d
Andreas Krühlmann <andreas@kruhlmann.dev>
2025-01-14 11:50:35 +0100

    lint

--- home/res/xmonad/xmonad.hs
+++ home/res/xmonad/xmonad.hs
@@ -128,16 +128,17 @@ myWorkspaces :: [String]
 myWorkspaces = ["Firefox", "Programming", "Instant Messaging", "Email", "Bitwarden", "Virtual Machines", "WINE Games", "Steam Games", "Settings"]
 
 workspaceWallpapers :: M.Map String FilePath
-workspaceWallpapers = M.fromList
-    [ ("Firefox", "~/img/lib/wallpaper/workspaces/1.png")
-    , ("Programming", "~/img/lib/wallpaper/workspaces/2.png")
-    , ("Instant Messaging", "~/img/lib/wallpaper/workspaces/3.png")
-    , ("Email", "~/img/lib/wallpaper/workspaces/4.png")
-    , ("Bitwarden", "~/img/lib/wallpaper/workspaces/5.png")
-    , ("Virtual Machines", "~/img/lib/wallpaper/workspaces/6.png")
-    , ("WINE Games", "~/img/lib/wallpaper/workspaces/7.png")
-    , ("Steam Games", "~/img/lib/wallpaper/workspaces/8.png")
-    , ("Settings", "~/img/lib/wallpaper/workspaces/9.png")
+workspaceWallpapers =
+  M.fromList
+    [ ("Firefox", "~/img/lib/wallpaper/workspaces/1.png"),
+      ("Programming", "~/img/lib/wallpaper/workspaces/2.png"),
+      ("Instant Messaging", "~/img/lib/wallpaper/workspaces/3.png"),
+      ("Email", "~/img/lib/wallpaper/workspaces/4.png"),
+      ("Bitwarden", "~/img/lib/wallpaper/workspaces/5.png"),
+      ("Virtual Machines", "~/img/lib/wallpaper/workspaces/6.png"),
+      ("WINE Games", "~/img/lib/wallpaper/workspaces/7.png"),
+      ("Steam Games", "~/img/lib/wallpaper/workspaces/8.png"),
+      ("Settings", "~/img/lib/wallpaper/workspaces/9.png")
     ]
 
 myFont = "-*-dejavu-*-*-*-*-16-*-*-*-*-*-*"
@@ -203,16 +204,17 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) =
            | (i, k) <- zip (XMonad.workspaces conf) [xK_1 .. xK_9],
              (f, m) <- [(W.view, 0), (W.shift, shiftMask)]
          ]
+
 updateWallpapers :: X ()
 updateWallpapers = do
-    ws <- gets windowset
-    let currentScreens = W.current ws : W.visible ws
-    let currentWorkspaces = map (W.tag . W.workspace) currentScreens
-    -- Retrieve wallpaper paths for current workspaces
-    let wallpapers = map (\wsName -> M.findWithDefault "" wsName workspaceWallpapers) currentWorkspaces
-    -- Set wallpapers using feh; ensure the number of wallpapers matches the number of monitors
-    when (not (null wallpapers)) $
-        safeSpawn "feh" ("--bg-scale" : wallpapers)
+  ws <- gets windowset
+  let currentScreens = W.current ws : W.visible ws
+  let currentWorkspaces = map (W.tag . W.workspace) currentScreens
+  -- Retrieve wallpaper paths for current workspaces
+  let wallpapers = map (\wsName -> M.findWithDefault "" wsName workspaceWallpapers) currentWorkspaces
+  -- Set wallpapers using feh; ensure the number of wallpapers matches the number of monitors
+  when (not (null wallpapers)) $
+    safeSpawn "feh" ("--bg-scale" : wallpapers)
 
 myMouseBindings (XConfig {XMonad.modMask = modMask}) =
   M.fromList $
